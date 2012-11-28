787fb62ada079b4f08860c16a93ca53bd73d852b
Quintin <quintinsmith@icloud.com>
2012-11-28 12:57:26 -0700

    adding two more files

---
+++ random.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: quintin.smith
+ * Date: 11/27/12
+ * Time: 4:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "How are you doing today";
