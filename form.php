654aa2b67185e79d4c9f19c8b4debcc26cf4b4cf
q45 <quintinsmith@gmail.com>
2012-12-28 16:19:28 -0700

    Added a form

---
+++ form.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: quintin.smith
+ * Date: 12/6/12
+ * Time: 9:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+print_r($_GET);
---
+++ lab1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+    echo "
+    <ul>
+        <li>Maxima</li>
+        <li>Honda</li>
+        <li>toyota</li>
+    </ul>
+    ";
+?>
+</body>
+</html>
